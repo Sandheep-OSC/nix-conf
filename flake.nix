6b74fe6df88c8f330f35ab001241c3535c8c8c9a
SuSSan@143 <budtut143@gmail.com>
2024-08-12 17:17:15 +0000

    initial commit

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    # NixOS official package source, using the nixos-23.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
+  let 
+   lib = nixpkgs.lib;
+   pkgs = nixpkgs.legacyPackages."x86_64-linux";
+   pkgs-unstable = nixpkgs-unstable.legacyPackages."x86_64-linux";
+   customProp = "disableBash";
+  in {
+    # Please replace my-nixos with your hostname
+    nixosConfigurations = {
+      my-nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          # Import the previous configuratiiiiiion.nix we used,
+          # so the old configuration file still takes effect
+          ./configuration.nix
+        ];
+        specialArgs = {
+          inherit pkgs-unstable;
+        };
+      };
+    };
+    homeConfigurations = {
+      demo = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          # Import the previous configuration.nix we used,
+          # so the old configuration file still takes effect
+          ./home.nix
+        ];
+        extraSpecialArgs = {
+          inherit customProp;
+          inherit pkgs-unstable;
+        };
+      };
+    };
+  };
+}
---
+++ sh.nix
@@ -0,0 +1,43 @@
+{ config, lib, customProp, pkgs, pkgs-unstable, ... }:
+
+let
+  # Define custom variables
+  customVariableTest = config.customVariableTest or "test";
+
+  # Define packages based on custom variables
+  myPackages = with pkgs; [
+    fzf git
+  ] ++ (if customVariableTest == "test" then [ pkgs.brave ] else []);
+in
+{
+  imports = [];
+
+  options = {
+    # Define my custom variables
+    customVariableTest = lib.mkOption {
+      type = lib.types.str;
+      default = "test";
+    };
+  };
+
+  config = {
+    customVariableTest = "";  # or use config.customVariableTest to provide default value
+
+    programs = {
+      bash = {
+        enable = if customProp == "enableBash" then true else false;
+      };
+      zsh = {
+        enable = true;
+      };
+    };
+
+    home = {
+      packages = myPackages ++ (
+        with pkgs-unstable; [
+          neovim
+        ]
+      );
+    };
+  };
+}
